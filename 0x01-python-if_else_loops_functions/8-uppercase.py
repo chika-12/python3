7763ad4bb326573f0275593105e5888514bbe9d3
root <root@DESKTOP-R7EUEM3.localdomain>
2022-11-30 13:34:18 +0000

    python code

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for x in str:
-        if ord(x) >= 97 and ord(x) <= 122:
-            x = chr(ord(x) - 32)
+        if ord(x) >= 65 and ord(x) <= 90:
         print("{}".format(x), end="")
     print()
