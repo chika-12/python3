8ecf486cda0716bf039d1ede5c675d66e0da1aa6
chika-12 <root@DESKTOP-R7EUEM3.localdomain>
2022-12-07 00:36:56 +0000

    py code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,8 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return ("None")
-    else:
-        return my_list[idx]
+    for i in my_list:
+        if idx < 0 or idx >= len(my_list):
+            return ("None")
+        else:
+            return my_list[idx]
