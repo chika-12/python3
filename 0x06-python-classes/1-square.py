5d1afe03757728effd6cc89f06575aa633cac7d2
chika-12 <root@DESKTOP-R7EUEM3>
2023-01-06 23:30:53 +0000

    python

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """Defines a square"""
 
+
 class Square:
 
     """Represents  a square"""
-
-
     def __init__(self, size):
         """initializing this squre"""
-
-
         self.__size = size
